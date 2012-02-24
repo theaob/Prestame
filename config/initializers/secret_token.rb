518ff906c9750cafc8228e7b0f46c6f2271ffd2c
Onur Baykal <theaob@gmail.com>
2012-02-24 19:07:46 +0200

    'recreate'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prestame::Application.config.secret_token = '167bb67d60d657830bd007d4e610623da62a938ce16e080e24432d16ea64ccb5738de3731ceae662622f4a785cad5e48aa393831638a4942c6bed22b5417d297'
