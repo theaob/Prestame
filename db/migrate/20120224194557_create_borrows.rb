1ff4944208ad509e1cd982aff5e8900e3497842a
Onur Baykal <theaob@gmail.com>
2012-02-24 22:13:09 +0200

    heroku fix

--- db/migrate/20120224194557_create_borrows.rb
+++ db/migrate/20120224194557_create_borrows.rb
@@ -1,13 +1,17 @@
 class CreateBorrows < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :borrows do |t|
-    	t.integer "giver", :null=>false
+    	t.integer "user_id", :null=>false
     	t.string "receiver", :null=>false
     	t.integer "amount", :null=>false
     	t.integer "interest"
-        t.boolean "paid"
+        t.boolean "paid", :default=>false
         t.date "duedate"
     	t.timestamps
     end
+  end
+  
+  def self.down
+  	drop_table :borrows
   end
 end
--- db/migrate/20120224194639_create_users.rb
+++ db/migrate/20120224194639_create_users.rb
@@ -1,9 +1,13 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :users do |t|
 		t.string "name", :null=>false
 		t.string "email", :null=>false
       t.timestamps
     end
+  end
+  
+  def self.down
+  	drop table :users
   end
 end
