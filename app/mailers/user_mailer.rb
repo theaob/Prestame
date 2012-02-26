7421a380e2321892416629c56c0b5709cc577060
Onur Baykal <theaob@gmail.com>
2012-02-26 23:12:30 +0200

    last minute fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,19 +4,25 @@ class UserMailer < ActionMailer::Base
 	def add_borrow(borrow)
 		@user = User.find(borrow.user_id)
 		@borrow = borrow
-		mail(:to=>@user.email,:subject=>"You've added a new lending")
+		if(@user.email)
+			mail(:to=>@user.email,:subject=>"You've added a new lending")
+		end
 	end
 	
 	def delete_borrow(borrow)
 		@user = User.find(borrow.user_id)
 		@borrow = borrow
-		mail(:to=>@user.email,:subject=>"You've deleted a lending")
+		if(@user.email)
+			mail(:to=>@user.email,:subject=>"You've deleted a lending")
+		end
 	end
 
 	def borrow_paid(borrow)
 		@user = User.find(borrow.user_id)
 		@borrow = borrow
+		if(@user.email)
 		mail(:to=>@user.email,:subject=>"You Got Paid")
+		end
 	end
 
 end
