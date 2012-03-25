4cca1f42659afcb47b5ce3f882943d3f9a3f3f0c
Onur Baykal <theaob@gmail.com>
2012-03-25 11:10:07 +0300

    New style

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Prestame::Application.config.secret_token = 'APPSECRETTOKEN'
+Prestame::Application.config.secret_token = '332903006bf8533710e2b488d627ad82'
